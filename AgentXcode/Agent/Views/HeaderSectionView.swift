35336daedc7dfbbe33a5c3e15bccd18a12b53e0e
Agent! for MacOS <todd@starplayrx.com>
2026-03-24 20:37:59 -0400

    Left-justify status items in toolbar capsule

--- AgentXcode/Agent/Views/HeaderSectionView.swift
+++ AgentXcode/Agent/Views/HeaderSectionView.swift
@@ -69,6 +69,7 @@ struct HeaderStatusView: View {
                     }
                 }
             }
+            Spacer(minLength: 0)
         }
         .padding(.horizontal, 8)
         .frame(minWidth: 200, alignment: .leading)
