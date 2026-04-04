4b99b028973b2da76d441a061e9db26631499e34
Agent! for MacOS <todd@starplayrx.com>
2026-04-04 01:51:17 -0400

    Overlay opacity: 0.95 dark mode, 1.0 light mode

--- AgentXcode/Agent/Views/ThinkingIndicatorView.swift
+++ AgentXcode/Agent/Views/ThinkingIndicatorView.swift
@@ -314,7 +314,7 @@ struct ThinkingIndicatorView: View {
                 .transition(.opacity)
             }
         }
-        .background(.ultraThinMaterial.opacity(0.95))
+        .background(.ultraThinMaterial.opacity(colorScheme == .dark ? 0.95 : 1.0))
         .onAppear {
             if let tab {
                 elapsed = tab.taskElapsed
